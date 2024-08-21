80304505e28749b54ef63a5ed92678749f95ef7f
Marten Manthey <martenmanthey1995@gmail.com>
2024-08-21 16:17:54 +0200

    testing api

--- src/js/rrze-faudir.js
+++ src/js/rrze-faudir.js
@@ -1,7 +1,8 @@
 jQuery(document).ready(function($) {
     console.log('RRZE FAUDIR JS from src directory');
 
-    $('#test-api-call').click(function() {
+    // Test API Call
+    $('#test-api-call').click(function () {
         $.ajax({
             url: 'https://api.fau.de/pub/v1/opendir/persons?limit=10&offset=1',
             type: 'GET',
@@ -10,12 +11,20 @@ jQuery(document).ready(function($) {
                 'Content-Type': 'application/json',
          
             },
-            success: function(response) {
-                $('#api-response').html('<pre>' + JSON.stringify(response, null, 2) + '</pre>');
+            success: function (response) {
+                if (response.success) {
+                    console.log('API Response Data:', response.data); // Log the actual data returned from the API
+                    $('#api-response').html('<pre>' + JSON.stringify(response.data, null, 2) + '</pre>');
+                } else {
+                    console.log('API Error:', response.data); // Log the error message
+                    $('#api-response').html('<p>Error: ' + response.data + '</p>');
+                }
             },
-            error: function(error) {
+            error: function (error) {
+                console.error('AJAX Error:', error); // Log the error object
                 $('#api-response').html('<p>Error occurred: ' + error.responseText + '</p>');
             }
         });
     });
+
 });
