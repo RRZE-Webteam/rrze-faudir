cff660f7f5d0d24f367c05d03a62c0a1212a1cf5
FitoreGashi <fitore.gashi@mondula.com>
2024-12-18 09:10:34 +0100

    remove unused code

--- faudir-block/block-faudir.php
+++ faudir-block/block-faudir.php
@@ -18,19 +18,9 @@ function rrze_faudir_block_init() {
 		return;
 	}
 
-
 }
 
 add_action('init', 'rrze_faudir_block_init');
-
-// Add admin notice if parent plugin is missing
-add_action('admin_init', function() {
-	if (!shortcode_exists('faudir')) {
-		add_action('admin_notices', function() {
-			echo '<div class="error"><p>The FAUDIR Block requires the RRZE-FAUDIR plugin to be installed and activated.</p></div>';
-		});
-	}
-});
 
 // Ensure styles are loaded in the editor
 add_action('enqueue_block_editor_assets', function() {
