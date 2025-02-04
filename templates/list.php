04e6fd4c88e7e471b533b6aee1be0cd1f946b5e3
Wolfgang Wiese <xwolf@xwolf.de>
2025-02-04 17:21:35 +0100

    Finetuning bei Listen

--- templates/list.php
+++ templates/list.php
@@ -185,20 +185,24 @@ if (!defined('ABSPATH')) {
              //           $output .= Debug::get_html_var_dump($show_fields);
              //           $output .= Debug::get_html_var_dump($hide_fields);
 
-                         $reihenfolge = ['function', 'url', 'mails', 'phones', 'street', 'zip', 'city', 'roompos', 'room', 'floor', 'address','faumap'];
+                        $reihenfolge = ['function', 'url', 'mails', 'phones', 'street', 'zip', 'city', 'roompos', 'room', 'floor', 'address','faumap'];
                         // Output Workplace Data
-                        $output .= FaudirUtils::getListOutput($workplaces,'span',__('Contactpoints', 'rrze-faudir'),'text-list icon',$show_fields,$hide_fields,$reihenfolge);
+                        $listdata = FaudirUtils::getListOutput($workplaces,'span',__('Contactpoints', 'rrze-faudir'),'text-list icon',$show_fields,$hide_fields,$reihenfolge);
 
-
+                        $socialoutput = '';
                         if (in_array('socialmedia', $show_fields) && !in_array('socialmedia', $hide_fields)) {                            
                             if (!empty($socials)) {        
-                                $output .= FaudirUtils::getListOutput($socials,'span',__('Portale', 'rrze-faudir'),'icon-list icon');
+                                $socialoutput = FaudirUtils::getListOutput($socials,'span',__('Portale', 'rrze-faudir'),'icon-list icon');
                             }
 
                         }
-                        if (!empty($output)) {
-                           $output = '('.$output.')';
+                        if (!empty($listdata)) {
+                             $output .= ', '.$listdata;
                         }
+                        if (!empty($socialoutput)) {
+                             $output .= ' '.$socialoutput;
+                        }
+                        
                         
                         // Output optional teasertext
                         if (in_array('teasertext', $show_fields) && !in_array('teasertext', $hide_fields)) {
