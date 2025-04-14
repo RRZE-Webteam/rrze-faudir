c4c8acf432426709c07981b94ec71326ae990408
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-04-14 10:52:16 +0200

    fix(CSS) - Fix SCSS for FAU-Einrichtungen – Resolve Panel Body issues
    Try resolving PanelBody display in FAU-Einrichtungen

--- build/block/index.asset.php
+++ build/block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'f545d63444d1ebf27ad9');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '8f171f1deed05503cb18');
