8afa24bf213ade04efff9166b62fd6f85a0706d7
Wolfgang Wiese <xwolf@xwolf.de>
2025-11-21 17:52:27 +0100

    spacing fix

--- build/blocks/faudir/index.asset.php
+++ build/blocks/faudir/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render', 'wp-url'), 'version' => '8f00b1d52aa083fde444');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render', 'wp-url'), 'version' => 'f29aa8d2fc16ec3e4cb8');
