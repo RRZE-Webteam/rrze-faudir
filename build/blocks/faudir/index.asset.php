d735ed6860934ccab2ba8461d8ea2ed459039a4e
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-12-08 17:01:24 +0100

    !feat(css) - Removes the grayscale overlay inside :before
    This update overwrites the gray filter-layer in the :before-Element inside faudirs css to prevent horizontal scroll on mobile devices in FAU-Elemental.

--- build/blocks/faudir/index.asset.php
+++ build/blocks/faudir/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render', 'wp-url'), 'version' => '287277544f0f2155fdbc');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render', 'wp-url'), 'version' => '0a09397ce63704630c19');
