0916cc23f87571f67816fd8b3602ff0cda695a58
Wolfgang Wiese <xwolf@xwolf.de>
2026-03-16 10:58:58 +0100

    Release 2.6

--- build/blocks/faudir/index.asset.php
+++ build/blocks/faudir/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render', 'wp-url'), 'version' => '5a9e254bd1c2d52e670d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render', 'wp-url'), 'version' => 'e7474448147d084770b4');
