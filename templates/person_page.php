d2d4405f1467deef211017ba32205549265736ee
Wolfgang Wiese <xwolf@xwolf.de>
2026-03-24 09:54:57 +0100

    Noch eine Absicherung gegen leere Anzeigenamen

--- templates/person_page.php
+++ templates/person_page.php
@@ -71,17 +71,17 @@ if (!defined('ABSPATH')) {
                 } ?>
                 <header class="profile-header">
                    <?php 
-
-                    $value = '';
-                    if (!empty($final_url) && in_array('link', $show_fields, true)) {
-                        $value .= '<a itemprop="url" href="'.esc_url($final_url).'">';     
+                    if (!empty($displayname)) {
+                        $value = '';
+                        if (!empty($final_url) && in_array('link', $show_fields, true)) {
+                            $value .= '<a itemprop="url" href="'.esc_url($final_url).'">';     
+                        }
+                        $value .= $displayname;
+                        if (!empty($final_url) && in_array('link', $show_fields, true)) {
+                            $value .= '</a>';
+                        }                        
+                        echo '<h1 id="'.esc_attr($aria_id).'">'.$value.'</h1>';
                     }
-                    $value .= $displayname;
-                    if (!empty($final_url) && in_array('link', $show_fields, true)) {
-                        $value .= '</a>';
-                    }                        
-                    echo '<h1 id="'.esc_attr($aria_id).'">'.$value.'</h1>';
-
                     if (!empty($contact) && in_array('organization', $show_fields, true)) {
                         echo '<p class="organisation_name">'. $contact->getOrganizationName($lang).'</p>';
                     }
