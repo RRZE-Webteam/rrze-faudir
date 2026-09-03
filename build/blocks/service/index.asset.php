59d13dd87a47191b214133d4a332d461122c208b
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-09-03 09:37:14 +0200

    fix(service-block): Fix Group behavior
    Fixes Group behavior in Frontend and Link color on inverted backgrounds.

--- build/blocks/service/index.asset.php
+++ build/blocks/service/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '912416360d64ac4f830e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ec3608b7c146de17f94c');
