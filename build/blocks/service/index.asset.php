1cc57f5a47b485b33a239a65c485756796b42014
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-09-03 09:26:30 +0200

    fix(service-block): Fix Colors in Groups

--- build/blocks/service/index.asset.php
+++ build/blocks/service/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a39c26541828b7988c7b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '912416360d64ac4f830e');
