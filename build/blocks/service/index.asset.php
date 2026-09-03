20fbed071005c52a99bd82c9e672b41c3a8f2353
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-09-03 09:22:00 +0200

    fix(service-block): Group behavior

--- build/blocks/service/index.asset.php
+++ build/blocks/service/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c2d0dc6d653fddaa605f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a39c26541828b7988c7b');
