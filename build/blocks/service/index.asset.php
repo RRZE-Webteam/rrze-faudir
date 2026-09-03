f9105c5c53bbeedd9572309a8ea7784979c47435
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-09-03 09:45:07 +0200

    fix(service-block): Fix Pseudo Element Behavior
    Wrestle the Pseudo Element

--- build/blocks/service/index.asset.php
+++ build/blocks/service/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ec3608b7c146de17f94c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'acbef9d100d76a8e4210');
