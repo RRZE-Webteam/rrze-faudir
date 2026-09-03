02921279bc6ed102545c3811ce9c7109a3002d14
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-09-03 09:02:02 +0200

    fix(service-block): Fixes the Overlay for Tablet & Mobile
    Fixes the Design for the Service block. Fixes an issue where the Image on tablet and mobile was shown in front of the Textbox, instead of behind.

--- build/blocks/service/index.asset.php
+++ build/blocks/service/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '57ff75c415645cc1b1b6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c2d0dc6d653fddaa605f');
